a46bbfd4526e983cd1a812fc2d55d797989fe6e9
R-Bicho <rogerio-bicho@outlook.com>
2022-12-22 12:01:41 -0300

    began construction of the main project

---
+++ main_mega_sena.py
@@ -0,0 +1,4 @@
+from funcs_mega_sena import (choose_one_number, total_rightness, validate_int,
+                             value_game, winning, winning_numbers)
+
+#  Define how many numbers user is gamble
